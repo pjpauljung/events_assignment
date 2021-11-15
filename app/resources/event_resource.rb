537ba5945ac06462c30c0d32c61507723bb821ad
anonymous anonymous <pjpauljung@gmail.com>
2021-11-15 16:21:14 +0000

    Generate graphiti resources for events

---
+++ app/resources/event_resource.rb
@@ -0,0 +1,16 @@
+class EventResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :event_name, :string
+  attribute :event_start, :datetime
+  attribute :event_end, :datetime
+  attribute :location, :string
+  attribute :host, :integer
+  attribute :details, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
