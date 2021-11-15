4bbef44037d2edebea39f9c8c3f130adcfd78622
anonymous anonymous <pjpauljung@gmail.com>
2021-11-15 16:20:49 +0000

    Display associated collections on show pages

--- app/controllers/rsvps_controller.rb
+++ app/controllers/rsvps_controller.rb
@@ -42,8 +42,14 @@ class RsvpsController < ApplicationController
   # DELETE /rsvps/1
   def destroy
     @rsvp.destroy
-    redirect_to rsvps_url, notice: 'Rsvp was successfully destroyed.'
+    message = "Rsvp was successfully deleted."
+    if Rails.application.routes.recognize_path(request.referrer)[:controller] != Rails.application.routes.recognize_path(request.path)[:controller]
+      redirect_back fallback_location: request.referrer, notice: message
+    else
+      redirect_to rsvps_url, notice: message
+    end
   end
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
