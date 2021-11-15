89f2dd3a4898d6be7d4dc8c893757ea869aa2ea9
anonymous anonymous <pjpauljung@gmail.com>
2021-11-15 16:20:40 +0000

    Whitelist columns in Rsvp for ActiveAdmin

--- app/admin/rsvps.rb
+++ app/admin/rsvps.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Rsvp do
 
+ permit_params :user_id, :event_id, :rsvp_selection
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
